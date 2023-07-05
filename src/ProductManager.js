3ecec6f452616744ccaec2d6bd25a3a16b55e575
Federico <nunez.fedema@gmmail.com>
2023-07-05 19:05:21 -0300

    desafio 3

---
+++ src/ProductManager.js
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+export class ProductManager{
+    constructor(path){
+        this.path=path;
+    };
+
+    async getProducts(){
+        return fs.promises.readFile(this.path,"utf-8");
+    };
+}
